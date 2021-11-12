69cf9769df094fb74dcd99d3dfe8fae19766cc20
Robert Kutt <robert.kutt@gmail.com>
2021-11-12 12:58:50 +0100

    Changes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,26 @@
 class BookmarksController < ApplicationController
 
-  def index
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+    redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,10 +15,20 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     # we need `restaurant_id` to associate review with corresponding restaurant
-    @list.save
+    if @list.save
     redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to lists_path
+  end
 
   private
 
